c7b178dee104a5ec96e0636d5ca9a5f2a44e53d0
guifrancisco <guifrancisco.almeida@outlook.com>
2023-11-08 23:31:07 -0300

    feat: :tada: kickstarting topets api

---
+++ src/main/java/com/topets/api/TopetsApplication.java
@@ -0,0 +1,13 @@
+package com.topets.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/topets/api/TopetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topets.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
